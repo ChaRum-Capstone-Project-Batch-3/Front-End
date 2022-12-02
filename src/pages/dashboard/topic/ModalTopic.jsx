6e586591019e31e2dc6b3936d0e2cefa5609660a
gozzafadillah <gozzafadillah@gmail.com>
2022-12-02 15:50:31 +0700

    fix : added validation

--- src/pages/dashboard/topic/ModalTopic.jsx
+++ src/pages/dashboard/topic/ModalTopic.jsx
@@ -8,17 +8,56 @@ const defaultData = {
   description: "",
   img: "",
 };
+const regexName = /^[A-Za-z ]*$/;
 
 const ModalTopic = (props) => {
   const [form] = Form.useForm();
   const [data, setData] = useState(defaultData);
+  const [errorMessages, setErrorMessages] = useState({
+    title: false,
+    description: false,
+    img: false,
+  });
+
   const onChangeHandler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
     setData({ ...data, [e.target.name]: e.target.value });
+    if (name === "title") {
+      if (regexName.test(value) && value !== "") {
+        setErrorMessages({
+          ...errorMessages,
+          title: false,
+        });
+      } else {
+        setErrorMessages({
+          ...errorMessages,
+          title: true,
+        });
+      }
+    }
+    if (name === "description") {
+      if (value !== "") {
+        setErrorMessages({
+          ...errorMessages,
+          description: false,
+        });
+      } else {
+        setErrorMessages({
+          ...errorMessages,
+          description: true,
+        });
+      }
+    }
   };
   return (
     <Modal
       open={props.isModalOpen}
-      onOk={props.handleOk}
+      onOk={
+        !errorMessages.title && !errorMessages.description
+          ? props.handleOk
+          : "disable"
+      }
       onCancel={props.handleCancel}
     >
       <Form
@@ -28,21 +67,38 @@ const ModalTopic = (props) => {
           layout: "vertical",
         }}
       >
-        <Form.Item style={{ fontWeight: "600" }} label="Topic Title">
+        <Form.Item
+          label="Topic Title"
+          validateStatus={!errorMessages.title ? "" : "error"}
+          help={
+            !errorMessages.title
+              ? ""
+              : "Should be combination alphabets and required"
+          }
+          style={{ fontWeight: "600" }}
+        >
           <Input
             placeholder="Write the title here.."
             value={data.title}
             name="title"
             onChange={onChangeHandler}
+            required={true}
           />
         </Form.Item>
-        <Form.Item style={{ fontWeight: "600" }} label="TextArea">
+        <Form.Item
+          style={{ fontWeight: "600" }}
+          label="TextArea"
+          validateStatus={!errorMessages.description ? "" : "error"}
+          help={!errorMessages.description ? "" : "required deskripsi"}
+          hasFeedback
+        >
           <TextArea
             rows={4}
             name="description"
             placeholder="Write the descriptions.."
             value={data.description}
             onChange={onChangeHandler}
+            allowClear
           />
         </Form.Item>
         <Form.Item style={{ fontWeight: "600" }} label="Upload Picture">

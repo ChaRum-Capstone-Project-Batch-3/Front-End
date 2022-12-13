bef2f27e284f182f24dc4a55c3bcb832ce78edc5
Diopranata01 <bagusdio01@gmail.com>
2022-12-13 15:48:28 +0800

    wip: slicing user details, dashboard

--- src/components/chart/DetailsChart.jsx
+++ src/components/chart/DetailsChart.jsx
@@ -12,64 +12,58 @@ import {
 
 const data = [
   {
-    name: "Page A",
+    name: "Harrasment",
     uv: 590,
     pv: 800,
     amt: 1400,
   },
   {
-    name: "Page B",
+    name: "Violation",
     uv: 868,
     pv: 967,
     amt: 1506,
   },
   {
-    name: "Page C",
+    name: "Child Abuse",
     uv: 1397,
     pv: 1098,
     amt: 989,
   },
   {
-    name: "Page D",
+    name: "Spam",
     uv: 1480,
     pv: 1200,
     amt: 1228,
   },
   {
-    name: "Page E",
+    name: "Annoying",
     uv: 1520,
     pv: 1108,
     amt: 1100,
-  },
-  {
-    name: "Page F",
-    uv: 1400,
-    pv: 680,
-    amt: 1700,
-  },
+  }
 ];
 
 export const DetailsChart = () => {
     return (
         <ComposedChart
-        style={{ 'display' : 'flex' }}
+        style={{ 
+          'display' : 'flex',
+         }}
+          className="chart-user"
           layout="vertical"
-          width={600}
-          height={250}
+          width={740}
+          height={180}
           data={data}
-          margin={{
-            top: 20,
-            right: 40,
-            bottom: 40,
-            left: 20
-          }}
+          margin={{ 
+            bottom: 30
+           }}
         >
           <CartesianGrid stroke="#f5f5f5" />
           <XAxis type="number" />
           <YAxis dataKey="name" type="category" scale="band" />
           <Tooltip />
           {/* <Legend layout="horizontal" verticalAlign="middle" align="right" height={90} width={10}/> */}
-          <Bar dataKey="pv" barSize={15} fill="#0074C1" />
+          <Bar dataKey="pv" fill="#0074C1" className="bar-size"/>
         </ComposedChart>
       );
 };
